58da8ed575b943e3d2a5a1f8600b1cd1de650ef8
Francisco Oliveira <fransoliveira@gmail.com>
2025-03-21 10:54:31 -0300

    Css atualizado

--- src/main/java/com/example/gestaoescolar/model/Professor.java
+++ src/main/java/com/example/gestaoescolar/model/Professor.java
@@ -1,21 +1,3 @@
-// package com.example.gestaoescolar.model;
-
-// import jakarta.persistence.*;
-
-// @Entity
-// public class Professor {
-
-//     @Id
-//     @GeneratedValue(strategy = GenerationType.IDENTITY)
-//     private Long id;
-
-//     private String nome;
-//     private String disciplina;
-
-//     // Getters e Setters
-// }
-
-
 
 package com.example.gestaoescolar.model;
 import jakarta.persistence.*;
