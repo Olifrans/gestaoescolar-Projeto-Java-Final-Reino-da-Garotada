2c49704a1a849045891433e6b51706395f6a8e5d
Francisco Oliveira <fransoliveira@gmail.com>
2025-04-12 11:37:16 -0300

    Repositorys

--- src/main/java/com/example/gestaoescolar/service/AlunoService.java
+++ src/main/java/com/example/gestaoescolar/service/AlunoService.java
@@ -1,37 +1,3 @@
-// package com.example.gestaoescolar.service;
-
-
-
-// import com.example.gestaoescolar.model.Aluno;
-
-// import com.example.gestaoescolar.repository.AlunoRepository;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Service;
-
-// import java.util.List;
-
-// @Service
-// public class AlunoService {
-
-//     @Autowired
-//     private AlunoRepository alunoRepository;
-
-//     public List<Aluno> listarTodos() {
-//         return alunoRepository.findAll();
-//     }
-
-//     public Aluno salvarAluno(Aluno aluno) {
-//         return alunoRepository.save(aluno);
-//     }
-
-//     public Aluno buscarPorId(Long id) {
-//         return alunoRepository.findById(id).orElse(null);
-//     }
-
-//     public void deletarAluno(Long id) {
-//         alunoRepository.deleteById(id);
-//     }
-// }
 
 
 
